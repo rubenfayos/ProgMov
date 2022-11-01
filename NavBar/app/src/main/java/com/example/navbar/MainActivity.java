82cbb588e7aa59af7e453afc0986c017e603b1f9
rubenfayos <fayosruben@gmail.com>
2022-11-01 21:02:36 +0100

    Al menos lo he conseguido hacer

---
+++ NavBar/app/src/main/java/com/example/navbar/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.navbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.navbar.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //fragmento predeterminado
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.home:
+                        replaceFragment(new HomeFragment());
+                        break;
+                    case R.id.mp3:
+                        replaceFragment(new mp3Fragment());
+                        break;
+                    case R.id.calendar:
+                        replaceFragment(new CalendarFragment());
+                        break;
+                }
+                return false;
+            }
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        //Cambiar fragmento
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+}
